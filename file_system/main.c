5d60199b36b287b759315864d8d22d5cf7d90092
Chigric <ggggg.writer.99@mail.ru>
2017-10-22 11:38:52 +0300

    stop, I need to some tests

--- file_system/main.c
+++ file_system/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <errno.h>
+
 
 #define MAX_BUF 1024
 #define KEY_N "-n"
@@ -14,28 +20,59 @@ struct File {
 //colors
 int main(int argc, char** argv)
 {
-	char buf[MAX_BUF];
+	char buf[MAX_BUF] = {0};
+        struct dirent *dp;
+        DIR *dirp;
+
+        if ((dirp = opendir(".")) == NULL) {
+                fprintf(stderr, "don't open .\n");
+                return 1;
+        }
+	int numbers_of_files = NULL;
+        while ((dp = readdir(dirp)) != NULL) {
+               	if (strcmp(dp->d_name, "file1.gm11") == 0) 
+			numbers_of_files = 1;
+		else
+			printf("no__");
+		printf("%s\n", (char*)dp->d_name);
+
+	}
+	closedir(dirp);
 	//name of save_file
 	char *gm11= "file1.gm11";
 	//open  save_file
 	FILE *oSFile = fopen(gm11, "a+");
         if(oSFile == NULL){
-                perror("FAIL WITH SAVE_FAIL!");
+                perror("FAIL WITH SAVE_FAIL!\n");
                 return 1;
         }
 	fseek(oSFile, 0, SEEK_SET);
-//	if (
-	//new_file
+	if (!numbers_of_files) {
+		fprintf(oSFile, "\n\tlist: 0\n\n---END_LIST\n");
+		printf("no gm11\n");
+	} else printf("yes gm11 %d\n", numbers_of_files);
+	//NEW_file
 	struct File new_file;
 	new_file.name = NULL;
 	new_file.text = NULL;
+	char** list_files;
 	if (strcmp((char*)KEY_N, argv[1]) == 0)
 	{
+		if (numbers_of_files) {
+			fscanf(oSFile, "%s%d", buf, &numbers_of_files);
+			int ptr = numbers_of_files;
+			list_files = (char**)malloc(ptr * sizeof(char*));
+			for (; ptr < 0; --ptr) {
+				if (fgets(buf, 100, oSFile) == NULL )
+                        		fprintf(stderr, "Bad read (list)\n");
+				list_files[ptr-1] = buf;
+			}
+		}		
 		new_file.name = argv[2];
 		//open new_file
 		FILE *oNFile = fopen(new_file.name, "r");
 		if(oNFile == NULL){
-	                perror("FAIL WITH NEW_FAIL!");
+	                perror("FAIL WITH NEW_FAIL!\n");
                 	return 1;
         	}
 		size_t s;
@@ -45,7 +82,7 @@ int main(int argc, char** argv)
 			s = fread(buf, sizeof(char), MAX_BUF, oNFile);
 			fwrite(buf, sizeof(char), s, oSFile);
 		}
-		fprintf(oSFile, "\nEND FILE ---");
+		fprintf(oSFile, "\nEND FILE ---\n");
 		//close new_file
 		fclose(oNFile);
 	} else if (strcmp((char*)KEY_D, argv[1]) == 0)
@@ -55,7 +92,10 @@ int main(int argc, char** argv)
 	{
 		//view list of files in save_file
 	}
-	printf("%d %s\n", argc, new_file.name);
+	int i;
+	for (i = 0; i < numbers_of_files; i++)
+		free(list_files[i]);
+	free(list_files);
 	//close save_file
 	fclose(oSFile);
 	return 0;
