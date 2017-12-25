c6172adaeca06762ffc674d4e651f244e4019949
Chigric <ggggg.writer.99@mail.ru>
2017-12-25 10:30:08 +0300

    fix file_system for OSX, :/

--- file_system/main.c
+++ file_system/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
+#ifdef __APPLE__
+#else
+	#include <malloc.h>
+#endif
 #include <stdint.h>
 #include <stdbool.h>
 
